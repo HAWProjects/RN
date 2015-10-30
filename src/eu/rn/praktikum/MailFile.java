02acbf6d2ebb34caa145209609dac0dc8af8c866
abt434 <abt434@141.22.40.128>
2015-10-30 16:13:40 +0300

    Aufgabe 1 ready

--- src/eu/rn/praktikum/MailFile.java
+++ src/eu/rn/praktikum/MailFile.java
@@ -3,65 +3,71 @@
  */
 package eu.rn.praktikum;
 
-
-import java.io.DataInputStream;
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import java.util.Properties;
 
 /**
  * @author Robert
- *		
+ * 
  */
 public class MailFile {
-	
-	private final String mailAdressReciever;
-	private final String filePath;
-	
-	private static final String ABSENDER_MAILADRESSE = "mail.mailadresse";
-	private static final String ABSENDER_BENUTZERNAME = "mail.benutzername";
-	private static final String ABSENDER_PW = "mail.passwort";
-	private static final String ABSENDER_HOSTNAME = "mail.hostname";
-	private static final String ABSENDER_PORTSMTP = "mail.portSMTP";
-	private static final String ABSENDER_PORTSMTPS = "mail.portSMTPS";
-	
-	private MyFileWriter myFileWriter;
-	
-	private String outputLogfileName = "";
-	private String outputLogfilePath = "";
-	
 
-	
+	private final String mailAdressReceiver;
+	private final String filePath;
+
+	private static String ABSENDER_MAILADRESSE = "mail.mailadresse";
+	private static String ABSENDER_BENUTZERNAME = "mail.benutzername";
+	private static String ABSENDER_PW = "mail.passwort";
+	private static String ABSENDER_HOSTNAME = "mail.hostname";
+	private static String ABSENDER_PORTSMTP = "mail.portSMTP";
+	private static String ABSENDER_PORTSMTPS = "mail.portSMTPS";
+	private static String MAIL_BODY = "mail.body";
+
+	private MyFileWriter myFileWriter;
+
+	private String outputLogfileName = "Logfile.txt";
+	private String outputLogfilePath = "files/";
+
+	private DataOutputStream dos;
+	private BufferedReader inFromServer;
+
 	/**
 	 * 
-	 * @param mailAdressReciever
+	 * @param mailAdressReceiver
 	 * @param filePath
 	 * @throws IOException
 	 */
-	public MailFile(String mailAdressReciever, String filePath) throws IOException {
+	public MailFile(String mailAdressReceiver, String filePath) throws IOException {
 		this.myFileWriter = new MyFileWriter(outputLogfileName, outputLogfilePath);
-		this.mailAdressReciever = mailAdressReciever;
+		this.mailAdressReceiver = mailAdressReceiver;
 		this.filePath = filePath;
 		Properties prop = new Properties();
 		try {
 			prop.load(getClass().getClassLoader().getResourceAsStream("mail.properties"));
-		} catch(IOException e) {
-			throw new FileNotFoundException("PropertieFile not found");
+		} catch (IOException e) {
+			throw new FileNotFoundException("PropertiesFile not found");
 		}
-		
+
 		String mailadresse = prop.getProperty(ABSENDER_MAILADRESSE);
 		String benutzername = prop.getProperty(ABSENDER_BENUTZERNAME);
 		String pw = prop.getProperty(ABSENDER_PW);
 		String absenderHostname = prop.getProperty(ABSENDER_HOSTNAME);
 		int port = Integer.parseInt(prop.getProperty(ABSENDER_PORTSMTP));
-		
-		erzeugeVerbindung(mailadresse, benutzername, pw, absenderHostname, port);
-		
+		String body = prop.getProperty(MAIL_BODY).replace("\n.", "\n..").replace("\r.", "\r.."); // body mit transparency
+
+		erzeugeVerbindung(mailadresse, benutzername, pw, absenderHostname, port, body);
+
 	}
-	
+
 	/**
 	 * 
 	 * @param String
@@ -75,61 +81,114 @@ public class MailFile {
 	 * @param int
 	 *            port
 	 */
-	private void erzeugeVerbindung(String mailadresse, String benutzername, String pw, String absenderHostname, int port) {
+	private void erzeugeVerbindung(String mailadresse, String benutzername, String pw, String absenderHostname,
+			int port, String body) {
 		try {
 			// Handshake
 			Socket socket = new Socket(absenderHostname, port);
-			DataInputStream dis = new DataInputStream(socket.getInputStream());
-			DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-	
-			
+			dos = new DataOutputStream(socket.getOutputStream());
+			inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
 			// transfer
 			// header + Body codieren
-			dos.writeBytes("HELO\n");
-			dos.writeBytes("MAIL From: " + Base64.decode(ABSENDER_MAILADRESSE) + "\n");
-			dos.writeBytes("RCPT To: " + Base64.decode(mailAdressReciever) + "\n");
-			dos.writeBytes("DATA\n");
-			dos.writeBytes("DATA\n");
-			// nachricht
-			dos.writeBytes("");
+			receiveFromServer();
+
+			sendToServer("EHLO " + absenderHostname);
+			receiveFromServer();
+
+			sendToServer("AUTH LOGIN");
+			sendToServer(Base64.encodeBytes(benutzername.getBytes()));
+			sendToServer(Base64.encodeBytes(pw.getBytes()));
+			receiveFromServer();
+
+			sendToServer("MAIL FROM: " + mailadresse);
+			receiveFromServer();
+			sendToServer("RCPT TO: " + mailAdressReceiver);
+			receiveFromServer();
+			sendToServer("DATA");
+			receiveFromServer();
 			
+			sendToServer("From: Deine Mudder");
+			sendToServer("To: Bastard");
+			sendToServer("Subject: Du riechst");
+			sendToServer("MIME-Version: 1.0");
+			sendToServer("Content-Type: multipart/mixed; boundary=8484636278363");
+			sendToServer("");
 			
-			dos.writeBytes("\n.\n");
-			// Schlie�en
-			dos.writeBytes("QUIT");
+			sendToServer("--8484636278363");
+			sendToServer("Content-Transfer-Encoding: quoted-printable");
+			sendToServer("Content-Type: text/plain");
+			sendToServer("");
+			sendToServer(body);
 			
-			String inLine;
-			while((inLine = dis.readLine()) != null) {
-				System.out.println("Antwort: " + inLine);
-				if(inLine.indexOf("Ok") != -1) {
-					break;
-				}
-			}
+			sendToServer("--8484636278363");
+			sendToServer("Content-Transfer-Encoding: base64");
+			sendToServer("Content-Type: image/jpg");
+			sendToServer("Content-Disposition: attachment; filename=putty.jpg");
+			sendToServer("");
 			
+			byte[] byteFile = Files.readAllBytes(Paths.get("files/putty.jpg"));
+			String encodedString = Base64.encodeBytes(byteFile);
+			sendToServer(encodedString);
+			sendToServer("--8484636278363--");
+
+			// terminieren
+			sendToServer("\r\n.");
+			receiveFromServer();
+			// schliessen
+			sendToServer("QUIT");
+
 			dos.close();
-			dis.close();
+			inFromServer.close();
 			socket.close();
-			
-		} catch(UnknownHostException e) {
+
+		} catch (UnknownHostException e) {
 			e.printStackTrace();
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
+
+	private void sendToServer(String message) {
+		try {
+			dos.writeBytes(message + "\r\n");
+			message = "Client: " + message;
+			myFileWriter.writeLine(message);
+			System.err.println(message);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void receiveFromServer() {
+		try {
+			do {
+				String message = "Server: " + inFromServer.readLine();
+				System.err.println(message);
+				myFileWriter.writeLine(message);
+			} while (inFromServer.ready());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		try {
 			new MailFile("robert.scheffel@haw-hamburg.de", "/RN_Praktikum/files/TestFile.txt");
-//			new MailFile(args[0], args[1]);
-		} catch(IOException e) {
+			// new MailFile(args[0], args[1]);
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 }
