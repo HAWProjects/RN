4838d7c70c607b1e228ec105d60790eac6e8bf70
Lutzi <fik@nurfuerspam.de>
2015-11-24 18:01:32 +0100

    fsdfsdf

--- src/eu/rn/praktikum2/ChatClient.java
+++ src/eu/rn/praktikum2/ChatClient.java
@@ -14,105 +14,127 @@ import java.util.Scanner;
  * @author abt434
  *
  */
-public class ChatClient {
-	
-	private String localHost;
-	private int serverPort;
-	private String userName;
-	private Socket socket;
-	private boolean connected;
-//	private MyFileWriter myWriter;
-	
-	
-	
-	
+public class ChatClient
+{
 
-	private DataOutputStream outToServer;
-	private BufferedReader inFromServer;
-	
-	
-	/**
-	 * @param localHost
-	 * @param serverPort
-	 * @param userName
-	 */
-	public ChatClient(String localHost,int serverPort) {
-		this.localHost = localHost;
-		this.serverPort = serverPort;
-		connected = false;
-		
-		startConnection();
-	}
+    private String localHost;
+    private int serverPort;
+    private String userName;
+    private Socket socket;
+    private boolean connected;
+    // private MyFileWriter myWriter;
 
-	/**
-	 * @throws IOException 
-	 * 
-	 */
-	private void startConnection() {
-		Scanner sc;
-		String input;
-		String messageFromServer;
-		
-		try {
-			socket = new Socket(localHost, serverPort);
-			outToServer = new DataOutputStream(socket.getOutputStream());
-			inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			connected = false;
-//			myWriter = new MyFileWriter("ChatLogFile.txt", "files/");
-			
-			sc = new Scanner(System.in);
-			System.out.println("Bitte Nutzernamen eingeben:");
-			userName = sc.nextLine();
-			
-			writeToServer("HELO");
-			if(readFromServer().equals("HELO")){
-				writeToServer("USER " +userName);
-				connected = true;
-			}
-			
-			while(connected){
-				
-				input = sc.nextLine();
-				writeToServer(input);
-				messageFromServer = readFromServer();
-				if(messageFromServer.startsWith("QUIT")){
-					connected = false;
-				}
-			}
-			
-			socket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
+    private DataOutputStream outToServer;
+    private BufferedReader inFromServer;
 
-	/**
-	 * @return
-	 * @throws IOException
-	 */
-	private String readFromServer() throws IOException {
-		String result = inFromServer.readLine();
-//		myWriter.writeLine("Server: " + result);
-		return result;
-	}
+    /**
+     * @param localHost
+     * @param serverPort
+     * @param userName
+     */
+    public ChatClient(String localHost, int serverPort)
+    {
+        this.localHost = localHost;
+        this.serverPort = serverPort;
+        connected = false;
 
-	/**
-	 * @param input
-	 * @throws IOException
-	 */
-	private void writeToServer(String input) throws IOException {
-		outToServer.writeBytes(input + "\r\n");
-//		myWriter.writeLine("Client: " + input);
-	}
+        startConnection();
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-//		new ChatClient("localhost",45619);
-		new ChatClient("192.168.178.23",45619);
+    /**
+     * @throws IOException
+     * 
+     */
+    private void startConnection()
+    {
+        Scanner sc;
+        String input;
+        String messageFromServer;
 
-	}
+        try
+        {
+            socket = new Socket(localHost, serverPort);
+            outToServer = new DataOutputStream(socket.getOutputStream());
+            inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            connected = false;
+            // myWriter = new MyFileWriter("ChatLogFile.txt", "files/");
+
+            sc = new Scanner(System.in);
+            System.out.println("Bitte Nutzernamen eingeben:");
+            userName = sc.nextLine();
+
+            writeToServer("HELO");
+            if (readFromServer().equals("HELO"))
+            {
+                writeToServer("USER " + userName);
+                connected = true;
+            }
+
+            Thread t = new Thread() {
+                public void run() {
+                    try
+                    {
+                        while(true)
+                        writeToServer(sc.nextLine());
+                    }
+                    catch (IOException e)
+                    {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            };
+            t.start();
+
+            while (connected)
+            {
+
+                messageFromServer = readFromServer();
+                System.out.println(messageFromServer);
+                if (messageFromServer.equals("Verbindung beendet"))
+                {
+                    connected = false;
+                }
+            }
+
+            socket.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * @return
+     * @throws IOException
+     */
+    private String readFromServer() throws IOException
+    {
+        String result = inFromServer.readLine();
+        // myWriter.writeLine("Server: " + result);
+        return result;
+    }
+
+    /**
+     * @param input
+     * @throws IOException
+     */
+    private void writeToServer(String input) throws IOException
+    {
+        outToServer.writeBytes(input + "\r\n");
+        // myWriter.writeLine("Client: " + input);
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // new ChatClient("localhost",45619);
+        new ChatClient("192.168.178.23", 45619);
+
+    }
 
 }
